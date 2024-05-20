360dd4e95976f26388b43a8e12799af0e3a867b4
João Silva <jops2205@gmail.com>
2024-05-20 20:44:44 +0100

    feat: add prisma orm and docker compose file

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3000),
+  DATABASE_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
